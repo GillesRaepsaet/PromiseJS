61210f7ddc425b2a42bcab96f75f62247d891dbb
GillesRaepsaet <gillesr6000@hotmail.com>
2022-05-14 09:37:12 +0200

    Promise

---
+++ exo2/script.js
@@ -0,0 +1,48 @@
+// VARIABLES
+
+const input = document.querySelector("#search-bar");
+const btn = document.querySelector("button");
+const select = document.querySelector("select");
+const options = document.querySelector("option");
+
+// LISTENERS
+btn.addEventListener("click", sendSearch);
+// FONCTION
+function sendSearch(e) {
+  e.preventDefault();
+  const value = input.value;
+  const country = select.value;
+  const fetchInput = (name, country) =>
+    fetch("https://api.agify.io/?name=" + name + "&country_id=" + country);
+
+  fetchInput(value, country)
+    .then((res) => res.json())
+    .then((data) => {
+      const div = document.createElement("div");
+      div.className = "result-search " + data.name;
+      const resultName = document.createElement("p");
+      const resultAge = document.createElement("p");
+      const resultCount = document.createElement("p");
+      const resultCountry = document.createElement("p");
+
+      localStorage.setItem("Name", data.name);
+      localStorage.setItem("Age", data.age);
+      localStorage.setItem("Count", data.count);
+      localStorage.setItem("Country", data.country_id);
+
+      resultName.innerHTML = "Name: " + data.name;
+      resultAge.innerHTML = "Age: " + data.age;
+      resultCount.innerHTML = "Count: " + data.count;
+      resultCountry.innerHTML = "Country: " + data.country_id;
+
+      document.body.appendChild(div);
+      div.appendChild(resultName);
+      div.appendChild(resultAge);
+      div.appendChild(resultCount);
+      div.appendChild(resultCountry);
+    });
+}
+// btn.onclick = () => {
+//   localStorage.setItem("search", input.value);
+//   localStorage.setItem("country", select.value);
+// };
